5939a20d0ba90cdb4c12231f65e7832ad9bf6bb2
Yuki Esaki <yesaki610@gmail.com>
2020-09-11 22:32:39 +0900

    テクスタグラム

--- TechtaGramL4S/ViewController.swift
+++ TechtaGramL4S/ViewController.swift
@@ -8,11 +8,83 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var cameraImageView: UIImageView!
+    
+    //元画像の宣言
+    var originalImage: UIImage!
+    
+    //フィルターの宣言
+    var filter: CIFilter!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func takePhoto() {
+        //カメラが使えるかの確認
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            //カメラを起動
+            let picker = UIImagePickerController()
+            picker.sourceType = .camera
+            picker.delegate = self
+            picker.allowsEditing = true
+            present(picker, animated: true, completion: nil)
+        } else {
+            //カメラが使えない時はエラーをコンソールに出す
+            print("error")
+        }
+    }
+    
+    @IBAction func savePhoto() {
+        UIImageWriteToSavedPhotosAlbum(cameraImageView.image!, nil, nil, nil)
+    }
+    
+    @IBAction func colorFilter() {
+        let filterImage: CIImage = CIImage(image: originalImage)!
+        
+        filter = CIFilter(name: "CIColorControls")!
+        filter.setValue(filterImage, forKey: kCIInputImageKey)
+        filter.setValue(1.0, forKey: "inputSaturation")
+        filter.setValue(0.5, forKey: "inputBrightness")
+        filter.setValue(2.5, forKey: "inputConrast")
+        
+        let ctx = CIContext(options: nil)
+        let cgImage = ctx.createCGImage(filter.outputImage!, from: filter.outputImage!.extent)
+        cameraImageView.image = UIImage(cgImage: cgImage!)
+    }
+    
+    @IBAction func openAlbum() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let picker = UIImagePickerController()
+            picker.sourceType = .photoLibrary
+            picker.delegate = self
+            picker.allowsEditing = true
+            present(picker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func snsPhoto() {
+        let shareText = "写真加工アプリ"
+        let shareImage = cameraImageView.image!
+        let activityItems: [Any] = [shareText, shareImage]
+        let activityViewContoroller = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+        let excludedActivityTypes = [UIActivity.ActivityType.postToWeibo, .saveToCameraRoll, .print]
+        activityViewContoroller.excludedActivityTypes = excludedActivityTypes
+        present(activityViewContoroller, animated: true, completion: nil)
+    }
+    
+    //カメラ、カメラロールを使った時に選択した画像をアプリ内に表示せるためのメソッド
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        cameraImageView.image = info[.editedImage] as? UIImage
+        
+        //cameraImageViewに表示されている写真が元画像
+        originalImage = cameraImageView.image
+        
+        //撮影後にアプリに戻る処理
+        dismiss(animated: true, completion: nil)
     }
 
 
